af4e36ed52f7943d717e28fb72d014d7013b9a26
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-11-12 17:00:36 +0700

    Add files via upload

---
+++ Bài tập 1/Variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  
+int a,b,c,d;
+  
+  printf ("((a + b) - c)*d ");
+  printf ("=");
+  
+  a=4,b=2,c=1,d=3;
+  printf (" ((%d + %d) - %d)*%d",a,b,c,d);
+  
+  int h=15;
+  printf (" = %d",h);
+  
+ 
+  getchar();
+  return 0;
+}
