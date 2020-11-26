3db4fadf6a1f1ddcb57283f887623da9693dc592
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-11-26 16:04:30 +0700

    Update pheptinh (2).C

--- Bài tập 1/pheptinh (2).C
+++ Bài tập 1/pheptinh (2).C
@@ -6,13 +6,16 @@ int main (void){
 	a=3;b=5;
 	
 	a+b; a-b; a*b; a/b;
+	
 	//khai bao bien thuong
 	double thuong;
+	
 	//ep kieu 
 	thuong =(double) a/b;
 	
 	//in ra man hinh 
 	printf("a\t b\t a+b\t a-b\t a*b\t a/b\n");
+	
 	//in ket qua ra man hinh
 	printf("%d\t %d\t %d\t %d\t %d\t %g",a,b,a+b,a-b,a*b,thuong);
 
