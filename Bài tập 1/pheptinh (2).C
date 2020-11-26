cf8f93bf464700a56049ab52f171e93b8711a4e4
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-11-26 14:50:54 +0700

    Add files via upload

---
+++ Bài tập 1/pheptinh (2).C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int a ;int b;
+	a=3;b=5;
+	
+	printf("a\t"); printf("b\t"); printf("a+b\t"); printf("a-b\t"); printf("a*b\t"); printf("a/b\n");
+	
+	
+	a+b; a-b; a*b; a/b;
+	
+	printf("%d\t",a); printf("%d\t",b);
+	 
+	printf("%d\t",a+b); printf("%d\t",a-b);
+	
+	printf("%d\t",a*b); 
+	
+	double c = 3;
+    double d = 5;
+    printf("%g", c/d); 
+	 
+	return 0;
+}
