1e04a9e07d5e600187a6f85debb8bdfa605bc1c6
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-10 15:05:40 +0700

    Add files via upload

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = -1;
+  //khiem ta a la duong hay am
+  if(a > 0) {
+    printf("%d la so duong",a);
+} else if(a == 0)	{
+	printf("a = 0")
+} else {
+    printf("%d la so am");
+}
+  printf("%d",a);
+  getchar()
+  return 1;
+}
