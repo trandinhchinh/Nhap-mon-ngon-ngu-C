6a7bea65a3c763f0c3ba2f056040a6621c2a1efa
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-17 16:40:10 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
+int main(){
 
-int main() {
-	//khai bao bien 
-	int thang;
-	char pt;
-	//hien thi ra man hinh
-	printf("vui long nhap thang");
-	//nhap tu ban phim
-	scanf("%c", &thang);
-	switch(thang){
-		case '1': printf("31 ngay");break;
-		
-        case '2': printf("28 ngay");break;
-        
-		case '3': printf("31 ngay");break;
-		
-		case '4': printf("30 ngay");break;
-		
-		case '5': printf("31 ngay");break;
-		
-		case '6': printf("30 ngay");break;
-		
-		case '7': printf("31 ngay");break;
-		
-		case '8': printf("31 ngay");break;
-		
-		case '9': printf("30 ngay");break;
-		
-		case '10': printf("31 ngay");break;
-		
-		case '11':	printf("30 ngay");break;
-		
-		case '12': printf("31 ngay");break;
-		
-		default: printf("vui long nhap ngay thang");
-	}
-
- 	return 0;
+  int thang;
+  printf ("vui long nhap thang ");
+  scanf ("%d", &thang);
+  switch(thang) {
+     case 2:
+     printf ("28 ngay");
+     break;
+  case 4 :
+     printf (" 30 ngay");
+      break;
+  case 6 :
+     printf (" 30 ngay");
+      break;
+  case 9 :
+     printf (" 30 ngay");
+      break;
+  case 11 :
+     printf (" 30 ngay");
+      break;
+      default: 
+       printf("31 ngay");
+    
+  }
+ 
+  
+  getchar();
+  return 0;
 }
