896c076a097bffb9b2c750064ac0bd568d0f0e31
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-17 16:07:44 +0700

    Update pheptinh.c

--- Bài tập 3/pheptinh.c
+++ Bài tập 3/pheptinh.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 int main(){
+  //khai bao bien a =5 vs b=5
   int a = 5, b = 5;
   char pt;
+  //hien thi ra man hinh
   printf ("vui long nhap phep tinh ");
+  //nhap tu ban phim
   scanf ("%c",&pt);
+  // cau lenh swwitch de su dung phep tinh
   switch(pt) {
      case '+': printf (" a + b = %d",a +b);
       break;
   	 case '-': printf (" a - b = %d",a -b);
       break;
+       
       default:
        printf(" nhap cong hoac tru ");
   }
