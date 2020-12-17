ec223ee608e0832274ac01ffc5ef946e560174de
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-17 16:57:50 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -3,25 +3,29 @@ int main(){
 
   int thang;
   printf ("vui long nhap thang ");
+  // nh?p phép tính c?ng ho?c tr?
   scanf ("%d", &thang);
   switch(thang) {
      case 2:
-     printf ("28 ngay");
-     break;
-  case 4 :
+      printf (" 28 ngay");
+      break;
+   case 4 :
+   case 6 :
+   case 9 :
+   case 11 :
      printf (" 30 ngay");
       break;
-  case 6 :
-     printf (" 30 ngay");
-      break;
-  case 9 :
-     printf (" 30 ngay");
-      break;
-  case 11 :
-     printf (" 30 ngay");
-      break;
-      default: 
-       printf("31 ngay");
+    case 1: 
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+	printf(" 31 ngay");
+	   break; 
+      default:
+       printf ( "vui long nhap tu thag 1 den thang 12");
     
   }
  
