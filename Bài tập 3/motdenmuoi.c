01e64b343d6bbf8730befb393696bb44c02963c4
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-24 14:19:39 +0700

    Add files via upload

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	//khai bao bien i
+	int i;
+  for( i = 1; i < 11; i++){
+    printf(" %d", i);
+    }
+  int count;
+  for(count = 1 ; count < 11; count++){
+    printf("\n 1 2 3 4 5 6 7 8 9 10");  
+  }
+  
+  return 0;
+}
---
+++ Bài tập 3/xin chao.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+	//khai bao bien
+	int count;
+
+	for(count = 1; count <=10; count++){
+		printf ("\n xin chao");
+	}
+	return 0;
+}
