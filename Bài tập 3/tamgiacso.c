2241c94973ad60d718e13ecf4761e9b245ad55b9
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2020-12-28 17:56:18 +0700

    Update tamgiacso.c

--- Bài tập 3/tamgiacso.c
+++ Bài tập 3/tamgiacso.c
@@ -7,9 +7,12 @@ int main(){
     //nhap du lieu tu ban phim
     scanf("%d", &n);
     //dieu kien cua n
-    for(int i = 1; i <= n; i++){
-       for(int a= 1; a <= n; a++) printf(" ");
-       for(int b = 1; b <= i; b++) printf("%d",b);
+    int i;
+    for( i = 1; i <= n; i++){
+    	int a;
+       for( a= 1; a <= n; a++) printf(" ");
+       int b;
+       for( b = 1; b <= i; b++) printf("%d",b);
         printf("\n");
     }
     return 0;
