1787ee6fb5f5b7828ff6d67f29c4ccea1d151f50
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-08 09:40:11 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -13,3 +13,18 @@ int main(void) {
 	printf("%d",s);
 return 0;
 }
+
+
+#include<stdio.h>
+int main(){
+    int a,n,s=0;
+    printf("Nhap so nguyen > 0: ");
+    scanf("%d",&n);
+    for(;n!=0;){
+        a= n%10;
+        n/= 10;
+        s+= a;
+    }    
+    printf("%d",s);
+     return 0;
+}
