37547bea10f48a814aaac21d24e6691ecd884e3a
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-14 15:42:46 +0700

    Update man_01.c

--- Bài tập 4/man_01.c
+++ Bài tập 4/man_01.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  int a[4];
+  int a[4];//khai bao mang a co 4 ngan
   a[0]=3;
   a[1]=5;
   a[2]=7;
   a[3]=2;
+  // tinh tong cac phan tu cua mang a
   int tong = 0,i=0;
   for(; i <4; i++){
     tong += a[i];
