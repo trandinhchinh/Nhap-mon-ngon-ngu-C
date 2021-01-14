000d78f2af3091bcf695805771e64d6a295f3d9f
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-14 15:36:12 +0700

    Update man_01.c

--- Bài tập 4/man_01.c
+++ Bài tập 4/man_01.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+  int a[4];
+  a[0]=3;
+  a[1]=5;
+  a[2]=7;
+  a[3]=2;
+  int tong = 0,i=0;
+  for(; i <4; i++){
+    tong += a[i];
+    }
+    printf("%d", tong);
+  return 0;
+}
