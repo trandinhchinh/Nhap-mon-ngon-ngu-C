c16187ef8e40d151b7a477c7a816d87180b2ab73
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-14 13:14:18 +0700

    Update kt_07.c

--- Bài tập 3/kt_07.c
+++ Bài tập 3/kt_07.c
@@ -1,14 +1,12 @@
 int main(void) {
 // khoi tao bien
   int a, b, c;
-  // nhap du lieu a, b, c tu ban phim
   printf("nhap canh a :");
   scanf("%d", &a);
   printf("nhap canh b :");
   scanf("%d", &b);
   printf("nhap canh c :");
   scanf("%d", &c);
-// tao dieu kien
   if(a+b > c || a+c > b || b+c > a){
     printf("%d, %d, %d la ba canh cua tam giac", a, b, c);
   }else{
