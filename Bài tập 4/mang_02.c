64a4e56129937b1ddd1c7ca6cddc80e216996425
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-19 14:34:29 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -19,3 +19,30 @@ int n;
     }
   return 0;
 }
+
+
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  printf("nhap n= ");
+  scanf("%d",&n);
+  int a[n];
+
+  for(int i=0;i < n ;i++){
+    printf("nhap phan tu thu %d: ",i+1);
+        scanf("%d", &a[i]);
+  }
+    printf("mang a: ");
+  for(int i=0; i < n; i++){
+      printf(" %d",a[i]);
+      }
+      printf("\nso chan tu mang a: ");
+  for(int i=0;i<n;i++){
+        if(a[i] % 2==0){
+        printf("%d ",a[i]);
+    }
+    }
+
+  return 0;
+}
