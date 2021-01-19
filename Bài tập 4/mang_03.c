c70fe1d87c02688a85e9b341af79118e182405e7
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-19 16:09:39 +0700

    Update mang_03.c

--- Bài tập 4/mang_03.c
+++ Bài tập 4/mang_03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
+  //khai bao va nhap n 
   int n,sc=0,sl=0,ko=0;
   printf("nhap n= ");
   scanf("%d",&n);
@@ -10,19 +11,22 @@ int main(void) {
     printf("nhap phan tu thu %d: ",i+1);
         scanf("%d", &a[i]);
   }
+  //hien thi cac phan tu cua mang a ra man hinh
     printf("mang a: ");
   for(int i=0; i < n; i++){
       printf(" %d",a[i]);
       }
-  
+      //hien thi ra man hinh so chan trong mang a
   printf("\nso chan tu mang a: ");
+
   for(int i=0;i<n;i++){
-        if(a[i] % 2==0 &&a[i]>0){
+        if(a[i] % 2==0 &&a[i]>0){ // lay cac so chan trong mang a va loai cac so 0 trong man a
         printf("%d ",a[i]);
         sc+=1;
         }
   }
-       printf("\nso le tu mang a: ");
+  // hien thi cac so le ra man hinh
+        printf("\nso le tu mang a: ");
   for(int i=0;i<n;i++){
         if(a[i] % 2!=0){
         printf("%d ",a[i]);
@@ -34,8 +38,8 @@ int main(void) {
         ko+=1;
     }
   }
-  printf("\nso cac so chan trong mang a:%d ",sc);
-  printf("\nso cac so le trong mang a:%d ",sl);
+  printf("\nso cac so chan trong mang a: %d ",sc);
+  printf("\nso cac so le trong mang a: %d ",sl);
   printf("\nso luong so 0: %d ",ko);
   return 0;
 }
