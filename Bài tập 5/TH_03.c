eb563ba0c95b8b03310d64466f4396b3fbfc6a4b
dinh chinh <72543355+trandinhchinh@users.noreply.github.com>
2021-01-23 09:27:23 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
-int a,b,c;// bien cuc bo khia bao a,b,c
-int solonnhat();
+float a,b,c;// bien cuc bo khia bao a,b,c
+float solonnhat();
 int main(){
   printf("Nhap so a = ");
-  scanf("%d",&a);//nhập số a từ ban phim
+  scanf("%f",&a);//nhập số a từ ban phim
   printf("Nhap so b = ");
-  scanf("%d",&b);//nhập số b từ ban phim
+  scanf("%f",&b);//nhập số b từ ban phim
   printf("Nhap so c = ");
-  scanf("%d",&c);//nhập số c từ ban phim
+  scanf("%f",&c);//nhập số c từ ban phim
   solonnhat();// cho ra KQ số lớn nhất
   return 0;
 }
-int solonnhat(){//Giá trị trả về của hàm solonnhat()
+float solonnhat(){//Giá trị trả về của hàm solonnhat()
   if(a>b &&a>c)//đk a>b và a>c
-   printf("%d",a);//nếu a là số lớn nhất thì in ra man hình
+   printf("%g",a);//nếu a là số lớn nhất thì in ra man hình
    
  if (b>a&& b>c)
- printf("%d",b);//nếu b là số lớn nhất thì in ra man hình
+ printf("%g",b);//nếu b là số lớn nhất thì in ra man hình
 
  if (c>a&&c>b)
- printf("%d",c);//nếu c là số lớn nhất thì in ra man hình
+ printf("%g",c);//nếu c là số lớn nhất thì in ra man hình
  return 0;
 }
